522b61aee9d1909d3a44afd320b8190755a6e9b5
JCJord <julinnn4k@live.com>
2024-02-24 14:59:59 -0300

    Spring boot initialization

---
+++ src/main/java/com/HuskyBank/HuskyBank/HuskyBankApplication.java
@@ -0,0 +1,13 @@
+package com.HuskyBank.HuskyBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuskyBankApplication.class, args);
+	}
+
+}

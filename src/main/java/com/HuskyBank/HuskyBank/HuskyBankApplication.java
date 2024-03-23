12122405fce5618fa0b5fd9ede973e889132fad8
JCJord <julinnn4k@live.com>
2024-03-23 11:08:24 -0300

    ADding dotenv lib to set env variables

--- src/main/java/com/HuskyBank/HuskyBank/HuskyBankApplication.java
+++ src/main/java/com/HuskyBank/HuskyBank/HuskyBankApplication.java
@@ -1,5 +1,6 @@
 package com.HuskyBank.HuskyBank;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HuskyBankApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Access environment variables
+		String dbUrl = dotenv.get("DB_URL");
+		String dbUsername = dotenv.get("DB_USERNAME");
+		String dbPassword = dotenv.get("DB_PASSWORD");
+
+		// Set environment variables
+		System.setProperty("DB_URL", dbUrl);
+		System.setProperty("DB_USERNAME", dbUsername);
+		System.setProperty("DB_PASSWORD", dbPassword);
 		SpringApplication.run(HuskyBankApplication.class, args);
 	}
 
